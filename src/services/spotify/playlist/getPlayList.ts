fb91fd9570636bcbe8260c24152014b89b005163
David Ng <david.ng.dev@gmail.com>
2020-07-11 15:59:42 +0800

    feat(service): get spotify playlist (#461)
    https://api.spotify.com/v1/playlists/{playlist_id}

---
+++ src/services/spotify/playlist/getPlayList.ts
@@ -0,0 +1,13 @@
+import { Fetcher } from '../typings/fetcher';
+
+const getPlayList: Fetcher<Spotify.Playlist> = async (
+  apiClient,
+  playListId: string,
+) => {
+  return apiClient.request({
+    url: `/playlists/${playListId}`,
+    method: 'GET',
+  });
+};
+
+export default getPlayList;
