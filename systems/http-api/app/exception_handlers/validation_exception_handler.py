91e27d8183ac9c20f33ba006d2b38af0b9a86060
David Ng <david.ng.dev@gmail.com>
2021-10-03 17:42:11 +0800

    refactor: swagger doc error response

--- systems/http-api/app/exception_handlers/validation_exception_handler.py
+++ systems/http-api/app/exception_handlers/validation_exception_handler.py
@@ -6,7 +6,7 @@ from fastapi.responses import JSONResponse
 
 async def validation_exception_handler(_, exc: RequestValidationError):
     return JSONResponse(
-        status_code=status.HTTP_400_BAD_REQUEST,
+        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
         content=jsonable_encoder({"errors": exc.errors(), "meta": {"body": exc.body}}),
     )
 
---
+++ systems/http-api/app/schemas/__init__.py
@@ -0,0 +1,1 @@
+from .error_response import ErrorResponse
