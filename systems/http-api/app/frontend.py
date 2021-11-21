b5171d1ecf0679fa189bc31133c804194681ecdd
David Ng <david.ng.dev@gmail.com>
2021-11-21 17:33:10 +0800

    fix: production empty page

--- systems/http-api/app/frontend.py
+++ systems/http-api/app/frontend.py
@@ -11,11 +11,19 @@ if os.getenv("FRONT_END_MANIFEST") is not None:
     manifest = Path(os.environ["FRONT_END_MANIFEST"]).read_text(encoding="utf-8")
     manifest = json.loads(manifest)
     templates = Jinja2Templates(directory="templates")
+    js_files = {k: v for (k, v) in manifest.items() if ".js" in k}
+    css_files = {k: v for (k, v) in manifest.items() if ".css" in k}
 
     @router.get(
         "/{file_path:path}", response_class=HTMLResponse, name="front_end_fallback"
     )
     def rest_of_route(request: Request):
         return templates.TemplateResponse(
-            "index.html", {"request": request, "manifest": manifest}
+            "index.html",
+            {
+                "request": request,
+                "manifest": manifest,
+                "js_files": js_files,
+                "css_files": css_files,
+            },
         )
